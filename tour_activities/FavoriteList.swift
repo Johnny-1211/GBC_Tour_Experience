1af41a688639940c458181c980d2b9f363df1580
Johnny <tamtingyuen@gmail.com>
2024-02-13 23:43:59 -0500

    feat: added toggle button for original tour list and favorite tour list

---
+++ tour_activities/FavoriteList.swift
@@ -0,0 +1,13 @@
+//
+//  Favorite.swift
+//  tour_activities
+//
+//  Created by Johnny Tam on 13/2/2024.
+//
+
+import Foundation
+
+
+class Favorite: ObservableObject {
+    @Published var favoritesList : [DataContent] = []
+}
---
+++ tour_activities/FavoriteListView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritelistView.swift
+//  tour_activities
+//
+//  Created by Johnny Tam on 13/2/2024.
+//
+
+import SwiftUI
+
+struct FavoritelistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritelistView()
+}
