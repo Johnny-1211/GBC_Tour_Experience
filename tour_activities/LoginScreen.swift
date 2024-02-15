427d13b339e38eb0a9ad6306a87619d6e2b4da06
Johnny <tamtingyuen@gmail.com>
2024-02-15 00:11:00 -0500

    integrate the login screen ui part

---
+++ tour_activities/LoginScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LoginScreen.swift
+//  tour_activities
+//
+//  Created by Johnny Tam on 15/2/2024.
+//
+
+import SwiftUI
+
+struct LoginScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginScreen()
+}
